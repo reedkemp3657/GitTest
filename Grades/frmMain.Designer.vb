4225b908df70b3da1b80f4f57674645b72a1e92d
Reed Kemp <reed.kemp3657@gmail.com>
2017-01-13 10:29:11 -0500

    commit

---
+++ Grades/frmMain.Designer.vb
@@ -0,0 +1,337 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstNames = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.lblProject = New System.Windows.Forms.Label()
+        Me.lblMidterm = New System.Windows.Forms.Label()
+        Me.lblAverage = New System.Windows.Forms.Label()
+        Me.lblHw2 = New System.Windows.Forms.Label()
+        Me.lblFinal = New System.Windows.Forms.Label()
+        Me.lblLetterGrade = New System.Windows.Forms.Label()
+        Me.lblHw1 = New System.Windows.Forms.Label()
+        Me.SaveFileDialog = New System.Windows.Forms.SaveFileDialog()
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SymmaryReportToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'lstNames
+        '
+        Me.lstNames.FormattingEnabled = True
+        Me.lstNames.Location = New System.Drawing.Point(12, 55)
+        Me.lstNames.Name = "lstNames"
+        Me.lstNames.Size = New System.Drawing.Size(120, 212)
+        Me.lstNames.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Names:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(138, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Project"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(138, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(61, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Homework "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(138, 190)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Midterm "
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(138, 245)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(58, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Average:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(138, 93)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(41, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Grades"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(146, 158)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(39, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Grade:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(138, 203)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(39, 13)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Grade:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(278, 245)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 13)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Grade:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(278, 203)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(39, 13)
+        Me.Label10.TabIndex = 11
+        Me.Label10.Text = "Grade:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(278, 190)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(29, 13)
+        Me.Label11.TabIndex = 12
+        Me.Label11.Text = "Final"
+        '
+        'lblProject
+        '
+        Me.lblProject.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblProject.Location = New System.Drawing.Point(216, 134)
+        Me.lblProject.Name = "lblProject"
+        Me.lblProject.Size = New System.Drawing.Size(50, 20)
+        Me.lblProject.TabIndex = 14
+        Me.lblProject.Text = "0"
+        Me.lblProject.UseMnemonic = False
+        '
+        'lblMidterm
+        '
+        Me.lblMidterm.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblMidterm.Location = New System.Drawing.Point(216, 190)
+        Me.lblMidterm.Name = "lblMidterm"
+        Me.lblMidterm.Size = New System.Drawing.Size(50, 20)
+        Me.lblMidterm.TabIndex = 15
+        Me.lblMidterm.Text = "0"
+        '
+        'lblAverage
+        '
+        Me.lblAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverage.Location = New System.Drawing.Point(216, 245)
+        Me.lblAverage.Name = "lblAverage"
+        Me.lblAverage.Size = New System.Drawing.Size(50, 20)
+        Me.lblAverage.TabIndex = 16
+        Me.lblAverage.Text = "0"
+        '
+        'lblHw2
+        '
+        Me.lblHw2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblHw2.Location = New System.Drawing.Point(278, 80)
+        Me.lblHw2.Name = "lblHw2"
+        Me.lblHw2.Size = New System.Drawing.Size(50, 20)
+        Me.lblHw2.TabIndex = 17
+        Me.lblHw2.Text = "0"
+        '
+        'lblFinal
+        '
+        Me.lblFinal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblFinal.Location = New System.Drawing.Point(340, 190)
+        Me.lblFinal.Name = "lblFinal"
+        Me.lblFinal.Size = New System.Drawing.Size(50, 20)
+        Me.lblFinal.TabIndex = 18
+        Me.lblFinal.Text = "0"
+        '
+        'lblLetterGrade
+        '
+        Me.lblLetterGrade.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblLetterGrade.Location = New System.Drawing.Point(340, 245)
+        Me.lblLetterGrade.Name = "lblLetterGrade"
+        Me.lblLetterGrade.Size = New System.Drawing.Size(50, 20)
+        Me.lblLetterGrade.TabIndex = 19
+        Me.lblLetterGrade.Text = "0"
+        '
+        'lblHw1
+        '
+        Me.lblHw1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblHw1.Location = New System.Drawing.Point(216, 80)
+        Me.lblHw1.Name = "lblHw1"
+        Me.lblHw1.Size = New System.Drawing.Size(50, 20)
+        Me.lblHw1.TabIndex = 21
+        Me.lblHw1.Text = "0"
+        Me.lblHw1.UseCompatibleTextRendering = True
+        '
+        'SaveFileDialog
+        '
+        Me.SaveFileDialog.FileName = "report"
+        Me.SaveFileDialog.Filter = "(*.txt)|*.txt|csv     files (*.cs)|*.csv"""
+        Me.SaveFileDialog.InitialDirectory = "C:"
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(441, 24)
+        Me.MenuStrip.TabIndex = 23
+        Me.MenuStrip.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenFileToolStripMenuItem, Me.SymmaryReportToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'OpenFileToolStripMenuItem
+        '
+        Me.OpenFileToolStripMenuItem.Name = "OpenFileToolStripMenuItem"
+        Me.OpenFileToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.OpenFileToolStripMenuItem.Text = "Open File"
+        '
+        'SymmaryReportToolStripMenuItem
+        '
+        Me.SymmaryReportToolStripMenuItem.Name = "SymmaryReportToolStripMenuItem"
+        Me.SymmaryReportToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.SymmaryReportToolStripMenuItem.Text = "Symmary Report"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'OpenFileDialog
+        '
+        Me.OpenFileDialog.FileName = "OpenFileDialog"
+        Me.OpenFileDialog.InitialDirectory = "c:\"
+        Me.OpenFileDialog.SupportMultiDottedExtensions = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(441, 412)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.lblHw1)
+        Me.Controls.Add(Me.lblLetterGrade)
+        Me.Controls.Add(Me.lblFinal)
+        Me.Controls.Add(Me.lblHw2)
+        Me.Controls.Add(Me.lblAverage)
+        Me.Controls.Add(Me.lblMidterm)
+        Me.Controls.Add(Me.lblProject)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstNames)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.MainMenuStrip = Me.MenuStrip
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Reed's Grade Report"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lstNames As System.Windows.Forms.ListBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents lblProject As System.Windows.Forms.Label
+    Friend WithEvents lblMidterm As System.Windows.Forms.Label
+    Friend WithEvents lblAverage As System.Windows.Forms.Label
+    Friend WithEvents lblHw2 As System.Windows.Forms.Label
+    Friend WithEvents lblFinal As System.Windows.Forms.Label
+    Friend WithEvents lblLetterGrade As System.Windows.Forms.Label
+    Friend WithEvents lblHw1 As System.Windows.Forms.Label
+    Friend WithEvents SaveFileDialog As System.Windows.Forms.SaveFileDialog
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SymmaryReportToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents OpenFileDialog As System.Windows.Forms.OpenFileDialog
+
+End Class
---
+++ Grades/frmMain.vb
@@ -0,0 +1,154 @@
+﻿'Reed Kemp'
+'10/20/2015'
+'CRN: 1588'
+Option Explicit On
+Option Strict On
+Imports System.IO
+Imports System.Math
+
+Public Class frmMain
+
+    Dim FirstName(24) As String
+    Dim LastName(24) As String
+    Dim grade(24) As String
+
+    Dim Hw1(24) As Integer
+    Dim Hw2(24) As Integer
+    Dim Project(24) As Integer
+    Dim Midterm(24) As Integer
+    Dim Final(24) As Integer
+    Dim average(24) As Integer
+    Dim ndx As Integer
+
+    Private Sub OpenFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenFileToolStripMenuItem.Click
+        OpenFileDialog.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog.FileOk
+        Dim inFile As StreamReader
+        OpenFileDialog.InitialDirectory = "C:\"
+        OpenFileDialog.Filter = "files (*.cs)|*.csv"
+
+        '   If (OpenFileDialog.ShowDialog() = DialogResult.Cancel) Then
+        'Exit Sub
+        '     Else
+
+        Dim record As String
+        Dim fields() As String
+        Dim avg As Double
+
+        inFile = File.OpenText(OpenFileDialog.FileName)
+        ndx = 0
+
+        Do While Not (inFile.EndOfStream)
+
+            record = (inFile.ReadLine)
+            fields = Split(record, ",")
+
+            FirstName(ndx) = fields(0)
+            LastName(ndx) = fields(1)
+            Hw1(ndx) = CInt(fields(2))
+            Hw2(ndx) = CInt(fields(3))
+            Project(ndx) = CInt(fields(4))
+            Midterm(ndx) = CInt(fields(5))
+            Final(ndx) = CInt(fields(6))
+
+            lstNames.Items.Add(LastName(ndx) & "," & FirstName(ndx))
+
+            avg = ((Hw1(ndx) + Hw2(ndx)) * 3) + (Project(ndx) * 4) + ((Midterm(ndx) + Final(ndx)) * 5)
+            avg = (avg / 20) + 0.5
+            average(ndx) = CInt(Truncate(avg))
+
+            grade(ndx) = GetFinalGrade(average(ndx))
+
+            ndx = ndx + 1
+        Loop
+
+        inFile.Close()
+        lstNames.SelectedIndex = 0
+
+        '   End If
+    End Sub
+    Private Sub SymmaryReportToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SymmaryReportToolStripMenuItem.Click
+        SaveFileDialog.ShowDialog()
+    End Sub
+
+    Private Sub SaveFileDialog_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles SaveFileDialog.FileOk
+        Dim outFile As StreamWriter
+        Dim savedfile As String = SaveFileDialog.FileName
+        SaveFileDialog.InitialDirectory = "C:"
+        SaveFileDialog.Filter = "txt files (*.txt)|*.txt|csv     files (*.cs)|*.csv"
+
+        '     If (SaveFileDialog.ShowDialog() = DialogResult.Cancel) Then
+        'Exit Sub
+        '   Else
+        Dim output As String = SaveFileDialog.FileName
+        Dim line As String
+        Dim Avg As Double
+
+        Avg = average.Sum / ndx
+        Avg = Avg + 0.5
+        outFile = File.CreateText(output)
+
+        outFile.WriteLine("Summary Report Created: " & Format(Now, "dddd, d MMM yyyy"))
+        outFile.WriteLine(" ")
+        outFile.WriteLine("Class Average:  " & Truncate(Avg))
+        outFile.WriteLine(" ")
+
+        For i = 0 To ndx - 1
+            line = FirstName(i) & " " & LastName(i) & " " & Hw1(i) & " " & Hw2(i) & " " & Project(i) & " " & Midterm(i) & " " & Final(i) & " ----> " & average(i) & " " & grade(i)
+
+            outFile.WriteLine(line)
+        Next
+
+        outFile.Close()
+        MsgBox("Summary Report has been created")
+        '   End If
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Dim response As Integer
+        response = MsgBox("Are you sure you want to exit", CType(MsgBoxStyle.YesNo + MsgBoxStyle.Exclamation, MsgBoxStyle), "Leaving?")
+        If (response = vbYes) Then Close()
+    End Sub
+
+    Private Sub lstNames_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstNames.SelectedIndexChanged
+
+        lblHw1.Text = Str(Hw1(lstNames.SelectedIndex))
+        lblHw2.Text = Str(Hw2(lstNames.SelectedIndex))
+        lblProject.Text = Str(Project(lstNames.SelectedIndex))
+        lblMidterm.Text = Str(Midterm(lstNames.SelectedIndex))
+        lblFinal.Text = Str(Final(lstNames.SelectedIndex))
+        lblAverage.Text = Str(average(lstNames.SelectedIndex))
+        lblLetterGrade.Text = grade(lstNames.SelectedIndex)
+    End Sub
+
+    Function GetFinalGrade(Average As Integer) As String
+
+        Select Case Average
+            Case 93 To 100
+                Return "A"
+            Case 90 To 92
+                Return "A-"
+            Case 87 To 89
+                Return "B+"
+            Case 83 To 86
+                Return "B"
+            Case 80 To 82
+                Return "B-"
+            Case 77 To 79
+                Return "C+"
+            Case 73 To 76
+                Return "C"
+            Case 70 To 72
+                Return "C-"
+            Case 67 To 69
+                Return "D+"
+            Case 60 To 66
+                Return "D"
+            Case Else
+                Return "E"
+        End Select
+    End Function
+End Class
